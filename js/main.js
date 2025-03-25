2614411ac0d763205d467a614e945349fe67a48f
scyyythe <caneteangel187@gmail.com>
2025-03-26 00:34:05 +0800

    admin sidebar only

--- js/main.js
+++ js/main.js
@@ -24,3 +24,21 @@ function showTab(tabId) {
       });
     });
   });
+
+
+
+//   addmin
+  
+     document.querySelectorAll(".menu-item").forEach((item) => {
+        item.addEventListener("click", function () {
+      
+          document.querySelectorAll(".menu-item").forEach((link) => link.classList.remove("active"));
+          this.classList.add("active");
+
+          document.querySelectorAll(".content-panel").forEach((panel) => panel.classList.remove("active"));
+
+       
+          const target = this.getAttribute("data-target");
+          document.getElementById(target).classList.add("active");
+        });
+      });
